f0964e46e2f6ddc75d70ad16b277115f2b763367
Jonathan Caburnay <jonathan@caburnay.dev>
2024-01-07 17:47:57 +0800

    fix: remove unnecessary refetch during refocusing

--- src/app/[username]/expense-tracker/layout.tsx
+++ src/app/[username]/expense-tracker/layout.tsx
@@ -4,7 +4,17 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function UserLayout(props: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={new QueryClient()}>
+    <QueryClientProvider
+      client={
+        new QueryClient({
+          defaultOptions: {
+            queries: {
+              refetchOnWindowFocus: false,
+            },
+          },
+        })
+      }
+    >
       {props.children}
     </QueryClientProvider>
   );
